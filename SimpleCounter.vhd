88a00232fdb422b2853b9a2bb834a5c94da6d919
J Kishan <kishan29j@gmail.com>
2019-07-10 16:58:52 +0530

    Counters - Extended
    Simple counter using RTl config

--- SimpleCounter.vhd
+++ SimpleCounter.vhd
@@ -2,22 +2,26 @@
 library ieee;
 	use ieee.std_logic_1164.all;
 	use ieee.numeric_std.all;
-
+    use ieee.fixed_pkg.all; 
+	use ieee.float_pkg.all;
 entity count is 
-	 port(clk, clr  : in std_logic;
-	      op : out signed(2 downto 0));
+	 port(Clk, clr  : in std_logic;
+	      op : out unsigned(2 downto 0));
      end count; 
 
   architecture behave of count is
-      signal count: integer;
-      begin
-          process(clk, clr)
-              begin
-                  if clr = '1' then 
-                     count <=0;
-                   elsif rising_edge(clk) then
-                       count <= count + 1;
-                   end if;
-               end process;
-               op <= to_signed(count,3);
+--      signal count: unsigned(2 downto 0);
+--      begin
+--          process(Clk, clr)
+--              begin
+--                  if clr = '1'  then 
+--                     count <= "000";
+--                   elsif rising_edge(Clk) then
+--                      count <= count + "1";
+--                   end if;
+--               end process;
+--               op <= count;
+		 signal count,add :ufixed(3 DOWNTO -3);
+		 begin 
+			count <= count + add;
            end behave;
