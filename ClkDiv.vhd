92792188be1c75937888b9427271b31a7849afc8
J Kishan <kishan29j@gmail.com>
2019-07-10 16:59:38 +0530

    Counter- Extended
    Counter using RTl

---
+++ ClkDiv.vhd
@@ -0,0 +1,30 @@
+ library ieee;
+	use ieee.std_logic_1164.all;
+	use ieee.Numeric_std.all;
+	use ieee.fixed_pkg.all; 
+	use ieee.float_pkg.all;
+ entity ClkDiv is 
+	port( clk,clr  :in std_logic;
+			DivOut : out std_logic);
+ end ClkDiv;
+
+architecture RTL of ClkDiv is
+	signal count: unsigned(3 downto 0); --Counter Varible
+      begin
+  clockcountdivide: process(Clk,clr)
+					begin
+					  if(clr = '1' or count = "1010") then
+						  count<= "0000";
+					elsif rising_edge(Clk) then
+						if count< "0101" then
+							DivOut <= '1';
+						 	count <= count + "1"; 
+						else 
+							DivOut <= '0';
+						 	count <= count + "1"; 
+						end if;
+						
+					end if;
+		end process clockcountdivide;  
+end RTL;
+ 
